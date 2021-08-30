2ab933865d8cbe59e30cf19622feb6bc69c2a910
Claudioub16 <claudioub16@gmail.com>
2021-08-30 13:58:30 -0300

    added the library updater

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const formModal = document.querySelector('.form-modal');
 
+// Storage
 const myLibrary = [{
   title: "1984",
   author: "George Orwell",
@@ -33,14 +34,39 @@ const formModalModifier = () => {
 };
 
 
+const addBook = (book, index) => {
+  const thisBook = document.querySelector('.book-list');
+  const ul = document.createElement('ul');
+  ul.classList.add('single-book')
+  for (let camp in book) {
+    const li = document.createElement('li');
+    let newCamp = camp.charAt(0).toUpperCase() + camp.slice(1)
+    li.textContent = `${newCamp}: ${book[camp]}`;
+    ul.appendChild(li);
+  }
+  thisBook.appendChild(ul);
+}
+
+
+const libraryUpdate = () => {
+  myLibrary.forEach((book, index) => addBook(book, index));
+}
+
+
+
+// Events
 
 document.querySelector('.new-book-btn').addEventListener('click', e => {
   formModal.style.display = 'flex'
 })
 
-document.querySelector('.close-button').addEventListener('click', formModalModifier())
+document.querySelector('.close-button').addEventListener('click', formModalModifier)
 
 document.querySelector('.submit-btn').addEventListener('click', e => {
   e.preventDefault()
   formModalModifier()
 })
+
+
+libraryUpdate()
+//document.addEventListener("DOMContentLoaded", libraryUpdate)
