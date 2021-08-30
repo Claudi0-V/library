12adc208e0eedbd546f56a8ef8d4923644eab4db
Claudi0-V <claudiov.dejesus@protonmail.com>
2021-08-30 19:56:03 -0300

    split the removeFromArray function in two, making possible to reuse the code

--- app.js
+++ app.js
@@ -18,12 +18,6 @@ const myLibrary = [{
   pages: "346",
   readed: true,
 },
-{
-  title: "Animal Farm",
-  author: "George Orwell",
-  pages: "300",
-  readed: false,
-},
 ];
 
 //Functions Area
@@ -33,28 +27,57 @@ const formModalModifier = () => {
   document.querySelector('.book-form').reset()
 };
 
+const changeIfBool = (camp) => {
+  if ( typeof camp === 'boolean') {
+    if (camp) return 'Yes';
+    return 'No;' 
+}
+  return camp;
+}
+
+const firstToUpperCase = srt => srt.charAt(0).toUpperCase() + srt.slice(1)
 
 const addBook = (book, index) => {
   const thisBook = document.querySelector('.book-list');
+  const deleteButton = document.createElement('button');
+  const xDiv = document.createElement('div');
   const ul = document.createElement('ul');
+  xDiv.textContent = '+';
+  xDiv.classList.add('book-close-button');
+  deleteButton.textContent = 'Change Read Status';
+  deleteButton.classList.add('change-status', 'modal-buttons');
   ul.classList.add('single-book')
+  ul.appendChild(xDiv);
   for (let camp in book) {
     const li = document.createElement('li');
-    let newCamp = camp.charAt(0).toUpperCase() + camp.slice(1)
-    li.textContent = `${newCamp}: ${book[camp]}`;
+    let newBookCamp = changeIfBool(book[camp]);
+    li.textContent = firstToUpperCase(`${camp}: ${newBookCamp}`);
     ul.appendChild(li);
   }
-  thisBook.appendChild(ul);
+  ul.appendChild(deleteButton);
+  thisBook.appendChild(ul); 
 }
 
+const libraryUpdate = () => myLibrary.forEach((book, index) => addBook(book, index));
 
-const libraryUpdate = () => {
-  myLibrary.forEach((book, index) => addBook(book, index));
+
+const findIndex = (e) => {
+  myLibrary.forEach((book, index) => {
+  let title = e[0].textContent === `Title: ${book.title}`;
+  let author = e[1].textContent === `Author: ${book.author}`;
+  let bookAutor = `Title: ${book.author}`
+  if(title && author) return index
+  })
 }
 
-
+const removeFromArray = (e) => {
+  let index = findIndex(e);
+  myLibrary.splice(index, 1);
+}
 
 // Events
+
+libraryUpdate()
 
 document.querySelector('.new-book-btn').addEventListener('click', e => {
   formModal.style.display = 'flex'
@@ -68,5 +91,17 @@ document.querySelector('.submit-btn').addEventListener('click', e => {
 })
 
 
-libraryUpdate()
-//document.addEventListener("DOMContentLoaded", libraryUpdate)
+
+const removeButton = document.querySelectorAll('.book-close-button');
+
+removeButton.forEach(button => button.addEventListener('click', (e) => {
+  e.target.parentElement.remove();
+  removeFromArray(e.target.parentElement.childNodes);
+
+}))
+
+const changeButton = document.querySelectorAll('.change-status');
+
+changeButton.forEach(button => button.addEventListener('click', (e) => {
+  e.target.parentElement.childNodes
+}))
