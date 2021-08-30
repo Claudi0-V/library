6748519bf0344a97e254fb4ed59c13e904e696b6
Claudi0-V <claudiov.dejesus@protonmail.com>
2021-08-30 05:50:32 -0300

    create basic html structure and style

---
+++ app.js
@@ -0,0 +1,24 @@
+const myLibrary = [{
+  title: "1984",
+  author: "George Orwell",
+  pages: "346",
+  readed: true,
+},
+{
+  title: "Animal Farm",
+  author: "George Orwell",
+  pages: "300",
+  readed: true,
+},{
+  title: "1984",
+  author: "George Orwell",
+  pages: "346",
+  readed: true,
+},
+{
+  title: "Animal Farm",
+  author: "George Orwell",
+  pages: "300",
+  readed: false,
+},
+];
